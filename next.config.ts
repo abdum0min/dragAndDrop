7c6e43d2e394fb907ffa00f3b88f5820821e7a47
abdum0min <abduganiabdurasulov5150@gmail.com>
2025-06-12 13:04:24 +0500

    ''

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.pravatar.cc', 'source.unsplash.com'], // ← Har ikkisi kiritilgan
+    domains: ['i.pravatar.cc', 'source.unsplash.com'],
   },
 }
 
